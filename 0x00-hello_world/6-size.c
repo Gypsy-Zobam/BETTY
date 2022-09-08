ca377d721d5ef0e86272510e9f137c00fc60de79
Gypsy-Zobam <zo_bam@yahoo.com>
2022-09-08 16:13:01 -0700

    6-size.c

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -13,10 +13,10 @@ int main(void)
 	long d;
 	long long e;
 	
-	printf("size of int = %d bytes\n", sizeof(a));
-	printf("size of char = %d bytes\n", sizeof(b));
-	printf("size of float = %d bytes\n", sizeof(c));
-	printf("size of long = %d bytes\n", sizeof(d));
-	printf("size of long long = %d bytes", sizeof(e));
+	printf("size of int : %d bytes\n", sizeof(a));
+	printf("size of char : %d bytes\n", sizeof(b));
+	printf("size of float : %d bytes\n", sizeof(c));
+	printf("size of long : %d bytes\n", sizeof(d));
+	printf("size of long long : %d bytes", sizeof(e));
 	return (0);
 }
