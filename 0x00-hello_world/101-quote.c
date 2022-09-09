65d253d3aab60c22f6a9dd293ae6536e5c579eaf
Gypsy-Zobam <zo_bam@yahoo.com>
2022-09-08 18:38:17 -0700

    101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -3,11 +3,11 @@
 #include <unistd.h>
 
 /**
- *main - Entry point
- *Return: Always 1 (Success)
+ * main -Entyr point
+ * Return: Always 1 (success)
  */
 int main(void)
 {
-	write(STDOUT_FILENO, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
+	write(STDOUT_FILENO, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
 	return (1);
 }
