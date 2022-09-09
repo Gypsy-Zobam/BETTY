9492cf78d758c04984d51c70f25747b5caa867b5
Gypsy-Zobam <zo_bam@yahoo.com>
2022-09-08 19:00:26 -0700

    101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,13 +1,15 @@
 #include <stdio.h>
-#include <stdlib.h>
 #include <unistd.h>
 
 /**
- * main -Entyr point
+ * main -print quote
+ *
+ * Description: print Dora's quote
  * Return: Always 1 (success)
  */
+
 int main(void)
 {
-	write(STDOUT_FILENO, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n" 59);
+	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
 	return (1);
 }
