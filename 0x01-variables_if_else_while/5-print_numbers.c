3a4f8ea2a86f13a41d70f70d1f590661cc41fdc0
Gypsy-Zobam <zo_bam@yahoo.com>
2022-09-09 16:23:14 -0700

    5-print_numbers.c

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - Single digit numbers of base 10
+ *
+ * Return: Always (Success)
+ */
+int main(void)
+{
+	int a = 0;
+
+	while (a < 10)
+	{
+	printf("value of a: %d\n", a);
+	a++;
+	}
+	return (0);
+}
