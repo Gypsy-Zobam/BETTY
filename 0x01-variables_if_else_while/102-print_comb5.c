23b12ffb90faf913ca4ddf00523a0a8a8aa6a057
Gypsy-Zobam <zo_bam@yahoo.com>
2022-09-09 21:29:07 -0700

    102-print_comb5.c

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,47 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+/**
+ * main - Possible different combinations of three digits
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int a, b, c, d;
+
+	for (a = 34; a <= 63; a++)
+	{
+		for (b = 34; b <= 63; b++)
+		{
+			for (c = 34; c <= 63; c++)
+			{
+				for (d = 34; d <= 63; d++)
+				{
+					if (((c + d) > (a + b) && c >= a) || a < c)
+					{
+						putchar(a);
+						putchar(b);
+						putchar(' ');
+						putchar(c);
+						putchar(d);
+
+					if (a + b + c + d == 252 && a == 53)
+					{
+					break;
+					}
+					else
+					{
+					putchar(',');
+					putchar(' ');
+					}
+					}
+				}
+			}
+		}
+	}
+
+	putchar('\n');
+	return (0);
+}
