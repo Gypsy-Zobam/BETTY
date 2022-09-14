bb1e236a7833116863e8f0f5c68550aad6c4522f
Gypsy-Zobam <zo_bam@yahoo.com>
2022-09-14 01:33:44 -0700

    3-islower.c

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -2,8 +2,8 @@
 
 /**
  * _islower - function that checks lowercase character
- *
- * Return: Always 0
+ *@c: check lowercase
+ * Return: 1 if c is lowercase, 0 if false
  */
 int _islower(int c)
 {
