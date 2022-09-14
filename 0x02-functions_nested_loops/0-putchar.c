5020e5c65dea422339e93a1e6b4e9eb6bc562ece
Gypsy-Zobam <zo_bam@yahoo.com>
2022-09-13 17:28:33 -0700

    0-putchar.c

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -8,9 +8,15 @@
  */
 int main(void)
 {
-	char *sh = "_putchar";
-
-	for (*sh = 0; *sh < 8; sh++)
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+	_putchar('\n');
 
 	return (0);
 }
