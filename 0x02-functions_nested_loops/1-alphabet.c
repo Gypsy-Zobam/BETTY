e2080f9b8180ebdb1584fd4bd418dec281095224
Gypsy-Zobam <zo_bam@yahoo.com>
2022-09-13 18:16:26 -0700

    1-alphabet.c

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * main - prints the alphabet in lowercase
+ *
+ */
+void print_alphabet
+
+{
+	char ch;
+
+	ch = 'a';
+	while (ch <= 'z')
+	{
+		_putchar(ch);
+		ch++;
+	}
+	_putchar('\n')
+}
