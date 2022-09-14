f3d9072df13843eaa84868b1f80138e8ceee19c3
Gypsy-Zobam <zo_bam@yahoo.com>
2022-09-14 08:37:13 -0700

    9-times_table.c

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * times_table - print the multiplication table
+ * Description: Print 0 - 9 multiplication table
+ * Return:
+ */
+void times_table(void)
+{
+	int j = 0, i;
+
+	for (j = '0'; j <= '9'; j++)
+	{
+		for (i = '0'; i <= '9'; i++)
+		{
+			_putchar(i);
+
+		}
+
+		_putchar('\n');
+		j++;
+	}
+}
