4d64fb5c1efff6fde58638873b8df379554b32dc
Gypsy-Zobam <zo_bam@yahoo.com>
2022-09-14 08:50:20 -0700

    9-times_table.c

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -9,17 +9,17 @@ void times_table(void)
 {
 	int j = 0, i;
 
-	for (j = '0'; j < '9'; j++)
+	for (j = '0'; j < '10'; j++)
 	{
-		for (i = '0'; i < '9'; i++)
+		for (i = '0'; i < '10'; i++)
 		{
 			_putchar(i);
 			_putchar(' ');
-			_putchar(',');
 
 		}
 
 		_putchar('\n');
+		_putchar(',');
 		j++;
 	}
 }
