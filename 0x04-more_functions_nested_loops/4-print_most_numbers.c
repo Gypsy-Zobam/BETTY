2c2622178ff25cdb032a96eb83cbe8eca8994163
Gypsy-Zobam <zo_bam@yahoo.com>
2022-09-15 09:53:43 -0700

    4-print_most_numbers.c

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -9,15 +9,13 @@ void print_most_numbers(void)
 {
 	char ch;
 
-	ch = '0';
-	while (ch <= '9')
+	for (ch = 0; ch < 10; ch++)
 	{
 		if ((ch == 2) || (ch == 4))
 		{
 			continue;
 		}
-		_putchar(ch);
-		ch++;
+		_putchar('0' + ch);
 	}
 	_putchar('\n');
 }
