3f5b6739356d832d0da22270f78260b628812d6f
Gypsy-Zobam <zo_bam@yahoo.com>
2022-09-15 04:02:22 -0700

    1-main.c

--- 0x03-debugging/1-main.c
+++ 0x03-debugging/1-main.c
@@ -12,12 +12,12 @@ int main(void)
 	printf("infinite loop incoming :(\n");
 
 	i = 0;
-	/**
-	 * while (i < 10)
-	 * {
-	 * putchar(1);
-	 * }
-	 */
+	
+	/*while (i < 10)*/
+	/*{*/
+	/*putchar(1);*/
+	/*}*/
+
 	printf("infinite loop avoided! \\o/\n");
 
 	return (0);
