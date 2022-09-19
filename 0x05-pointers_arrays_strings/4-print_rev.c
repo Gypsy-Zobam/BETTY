46a7f9390f02b198887c1dc223ad3fc1a5fa9c42
Gypsy-Zobam <zo_bam@yahoo.com>
2022-09-19 16:30:35 -0700

    4-print_rev.c

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -9,13 +9,12 @@
 void print_rev(char *s)
 {
 	int length, last;
-
-	length = 0; 
+	
+	length = 0;
 	while (s[length] != '\0')
 	{
 		length++;
 	}
-	
 	last = length - 1;
 	for (; last >= 0; last--)
 	{
