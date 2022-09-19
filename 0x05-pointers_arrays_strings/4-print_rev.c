ecea85b52fd8ba5bc1debb349367b1dcfaa13847
Gypsy-Zobam <zo_bam@yahoo.com>
2022-09-19 16:16:56 -0700

    4-print_rev.c

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * print_rev - pinrts a string, in reverse, following by a new line
+ * @s: pointer
+ * Return: Always 0
+ */
+
+void print_rev(char *s)
+{
+	int length, last;
+
+	for (length = 0; s[length] != '\0'; length++)
+		last = length - 1;
+	for (; last >= 0; last--)
+	{
+		_putchar(s[last]);
+	}
+	_putchar('\n');
+}
