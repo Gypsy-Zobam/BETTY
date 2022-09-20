da227db510e5bb7000c41a1c0b79c1877ad91552
Gypsy-Zobam <zo_bam@yahoo.com>
2022-09-19 17:57:22 -0700

    7-puts_half.c

--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -1,4 +1,4 @@
-#define _main_h_
+:#define _main_h_
 
 int _putchar(char c);
 void reset_to_98(int *n);
@@ -7,3 +7,4 @@ int _strlen(char *s);
 void print_rev(char *s);
 void rev_string(char *s);
 void puts2(char *str);
+void puts_half(char *str);
