b24b99d34758a4404aa4607eebdd3b335ee017e3
Gypsy-Zobam <zo_bam@yahoo.com>
2022-09-21 06:10:06 -0700

    0-strcat.c

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * _strcat - concatenates two strings.
+ * @src: source of string
+ * @dest: destination of string
+ * Return: Always null
+ */
+
+char *_strcat(char *dest, char *src)
+{
+	_strcat(dest, src);
+
+	return (0);
+}
---
+++ 0x06-pointers_arrays_strings/main.h
@@ -0,0 +1,4 @@
+#define _main_h_
+
+int _putchar(char c);
+char *_strcat(char *dest, char *src);
