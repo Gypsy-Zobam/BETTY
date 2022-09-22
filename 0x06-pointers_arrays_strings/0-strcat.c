f26ab26f03610d133ae181cccb0ac6bbdc5d835e
Gypsy-Zobam <zo_bam@yahoo.com>
2022-09-21 20:02:17 -0700

    0-strcat.c

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -4,12 +4,23 @@
  * _strcat - concatenates two strings.
  * @src: source of string
  * @dest: destination of string
- * Return: Always null
+ * Return: value *dest
  */
-
 char *_strcat(char *dest, char *src)
 {
-	_strcat(dest, src);
+	int x = 0;
+	int y = 0;
 
-	return (0);
+	while (dest[x] != '\0')
+	{
+		x++;
+	}
+	while (src[y] != '\0')
+	{
+		dest[x] = src[j];
+		y++;
+		x++;
+	}
+	dest[x] = '\0';
+	return (dest);
 }
