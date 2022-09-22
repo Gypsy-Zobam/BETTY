2579daf576bc5fc2623309522d4c7885028c28cd
Gypsy-Zobam <zo_bam@yahoo.com>
2022-09-22 10:39:42 -0700

    100-rot13.c

--- 0x06-pointers_arrays_strings/100-rot13.c
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -3,31 +3,28 @@
 /**
  * rot13 - Encoding a string using rot13.
  * One if, Two loops only...
- * @n: input pointer
+ * @v: input pointer
  * Return: decrypted string
  */
 
-char *rot13(char *n)
+char *rot13(char *v)
 {
-	int x, rot_c = 13, y = 0;
-	char toswap[] = {'A', 'N', 'a', 'n', 'B', 'O', 'b', 'o', 'C', 'P',
-		'c', 'p', 'D', 'Q', 'd', 'q', 'E', 'R', 'e', 'r', 'F', 'S', 'f',
-		's', 'G', 'T', 'g', 't', 'H', 'U', 'h', 'u', 'I', 'V', 'i', 'v',
-		'J', 'W', 'j', 'w', 'K', 'X', 'k', 'x', 'L', 'Y', 'l', 'y', 'M',
-		'Z', 'm', 'z'};
+	int x;
+	char rot13[] = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+	char ROT13[] = "nopqrstuvwxyzabcdefghijklmNOPQRSTUVWXYZABCDEFGHIJKLM";
+	char *ptr = v;
 
-	while (n[y] != '\0')
+	while (*v)
 	{
-		for (x = 0; x <= 51; x++)
+		for (x = 0; x <= 52; x++)
 		{
-			if (n[y] == toswap[x])
+			if (*v == rot13[x])
 			{
-				n[y] = n[y] + rot_c;
-				x = 51;
+				*v = ROT13[x];
+				break;
 			}
-			rot_c = rot_c * -1;
 		}
-		y++;
+		v++;
 	}
-	return (n);
+	return (ptr);
 }
