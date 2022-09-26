0f4e3caaddda74b8387d2d8189be6dd1a46bd6b2
Gypsy-Zobam <zo_bam@yahoo.com>
2022-09-26 10:10:51 -0700

    0-memset.c

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -10,12 +10,9 @@
 
 char *_memset(char *s, char b, unsigned int n)
 {
-	unsigned i = 0;
+	char *p = s;
 
-	while (i < n)
-	{
-		*(s + i) = b;
-		i++;
-	}
+	for (; n != 0; p++, n--)
+		*p = b;
 	return (s);
 }
