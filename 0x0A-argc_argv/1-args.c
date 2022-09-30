74d9e6ba116c7631358c831b2d6ce17bb2303769
Gypsy-Zobam <zo_bam@yahoo.com>
2022-09-30 11:36:18 -0700

    1-args.c

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -10,8 +10,16 @@
 
 int main(int argc, char *argv[])
 {
-	printf("argc = %d\n", argc);
-	printf("Let's see what is in argv[]\n");
+	int i;
+
+	if (argc == 1)
+		printf("%d\n", argc - n);
+	else
+	{
+		for(i = 0; *argc; i++, argv++);
+
+		printf("%d\n", i - 1);
+	}
 
 	return (0);
 }
