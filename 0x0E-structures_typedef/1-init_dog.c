7c9b7b2dfd9f0dc2de56d27507cf204d6f1a70d1
Gypsy-Zobam <zo_bam@yahoo.com>
2022-10-10 08:32:24 -0700

    1-init_dog.c

--- 0x0E-structures_typedef/1-init_dog.c
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -13,9 +13,9 @@ void init_dog(struct dog *d, char *name, float age, char *owner)
 {
 	if (d)
 	{
-		d->name = "poppy";
-		d->age = 3.5;
-		d->owner = "Bob";
+		d->name = name;
+		d->age = age;
+		d->owner = owner;
 	}
 }
 
